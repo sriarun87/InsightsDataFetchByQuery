c23f02eaf564ed69db7d204ce618420ebe7dd3a7
Arun Kumar Vinayagam <arun_kumar_vinayagam@homedepot.com>
2019-04-09 07:32:18 -0400

    adding config file to consume the configuration values

---
+++ config.class.php
@@ -0,0 +1,9 @@
+<?php
+
+Class Config {
+  
+  public $X_QUERY_KEY = "#KEY";
+  public $ORG_ACCOUNT_ID = "#ACC_ID";
+
+}
+?>
--- fetchDataByQuery.php
+++ fetchDataByQuery.php
@@ -2,17 +2,21 @@
 ini_set('memory_limit', '256M');
 date_default_timezone_set('America/New_York');
 
-include 'commons.class.php';
+include_once 'commons.class.php';
+include_once 'config.class.php';
 
+$config = new Config;
 $common = new Commons;
+
+// explode cmd arguments
 $cmdArguments = $common->explodeArguments($argv);
 
 // set headers
 $common->setHeaders("Accept: application/json");
-$common->setHeaders("X-Query-Key: <<X_QUERY_KEY>>");
+$common->setHeaders("X-Query-Key: " . $config->X_QUERY_KEY);
 
 // set curl url
-$common->curl_url = "https://insights-api.newrelic.com/v1/accounts/<<ORG_ACCOUNT_ID>>/query?nrql=";
+$common->curl_url = "https://insights-api.newrelic.com/v1/accounts/" . $config->ORG_ACCOUNT_ID . "/query?nrql=";
 if($cmdArguments['query']) {
     $common->curl_url = $common->curl_url . $cmdArguments['query'];
 }
